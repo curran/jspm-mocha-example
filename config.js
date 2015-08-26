070bee5ab0b90ec2489c17fccd2ed2f6119a6724
Christian Rackerseder <christian.rackerseder@mobilexag.de>
2015-08-26 16:03:21 +0200

    Added lodash mock
    Only for demonstration. Lodash usual does not need to be mocked.

--- config.js
+++ config.js
@@ -16,42 +16,21 @@ System.config({
   map: {
     "babel": "npm:babel-core@5.8.22",
     "babel-runtime": "npm:babel-runtime@5.8.20",
-    "chai": "npm:chai@3.2.0",
     "core-js": "npm:core-js@1.1.1",
-    "mocha": "npm:mocha@2.2.5",
-    "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.4.3"
-    },
+    "lodash": "npm:lodash@3.10.1",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:buffer@3.4.3": {
-      "base64-js": "npm:base64-js@0.0.8",
-      "ieee754": "npm:ieee754@1.1.6",
-      "is-array": "npm:is-array@1.0.1"
-    },
-    "npm:chai@3.2.0": {
-      "assertion-error": "npm:assertion-error@1.0.1",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "deep-eql": "npm:deep-eql@0.1.3",
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0",
-      "type-detect": "npm:type-detect@1.0.0"
-    },
     "npm:core-js@1.1.1": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:deep-eql@0.1.3": {
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "type-detect": "npm:type-detect@0.1.1"
-    },
-    "npm:mocha@2.2.5": {
-      "css": "github:systemjs/plugin-css@0.1.14"
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,25 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import System from 'systemjs';
 import '../config.js';
 
+chai.use(sinonChai);
+
 describe('myModule', () => {
-  let myModule;
+  let _, myModule;
 
   before((done) => {
-    System.import('./lib/myModule.js')
+    System.import('lodash')
+      .then((lodash) => {
+        _ = lodash;
+        // Mock lodash library
+        sinon.spy(_, 'camelCase');
+        System.set(System.normalizeSync('lodash'), System.newModule({
+          default: _
+        }));
+      })
+      .then(() => System.import('./lib/myModule.js'))
       .then((mod) => myModule = mod)
       .then(() => done())
       .catch((err) => console.error(err));
@@ -14,7 +27,8 @@ describe('myModule', () => {
 
   describe('Module Loading', () => {
     it('should load', () => {
-      expect(myModule['default']).to.equal('myModule works!');
+      expect(myModule['default']).to.equal('myModuleWorks');
+      expect(_.camelCase).to.have.been.calledWith('myModule works!');
     });
   });
 
